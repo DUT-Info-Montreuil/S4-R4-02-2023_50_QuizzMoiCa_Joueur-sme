d50548a6f11167ab3532353a655850ba84f41597
Armandpro12 <chaplainduretarmandpro@gmail.com>
2023-05-09 16:47:19 +0200

    add JoueurDTO.java, CreerJoueur.java, Langues.java + tree folders

---
+++ src/main/java/fr/iut/montreuil/S4_R4_02_2023/num_50/joueur_sme/entities/dto/JoueurDTO.java
@@ -0,0 +1,67 @@
+package fr.iut.montreuil.S4_R4_02_2023.num_50.joueur_sme.entities.dto;
+
+import com.sun.tools.javac.code.Attribute;
+import fr.iut.montreuil.S4_R4_02_2023.num_50.joueur_sme.resources.Langues;
+
+import java.util.ArrayList;
+
+public class JoueurDTO {
+    private String pseudo;
+    private String prenom;
+    private int anneeNaissance;
+    private ArrayList<String> hobbies;
+    private Langues langue;
+    private int score;
+
+    public JoueurDTO(){
+
+    }
+
+    public String getPseudo() {
+        return pseudo;
+    }
+
+    public void setPseudo(String pseudo) {
+        this.pseudo = pseudo;
+    }
+
+    public String getPrenom() {
+        return prenom;
+    }
+
+    public void setPrenom(String prenom) {
+        this.prenom = prenom;
+    }
+
+    public int getAnneeNaissance() {
+        return anneeNaissance;
+    }
+
+    public void setAnneeNaissance(int anneeNaissance) {
+        this.anneeNaissance = anneeNaissance;
+    }
+
+    public ArrayList<String> getHobbies() {
+        return hobbies;
+    }
+
+    public void setHobbies(String hobbies) {
+        this.hobbies = hobbies;
+    }
+
+    public Langues getLangue() {
+        return langue;
+    }
+
+    public void setLangue(Langues langue) {
+        this.langue = langue;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
+}
---
+++ src/main/java/fr/iut/montreuil/S4_R4_02_2023/num_50/joueur_sme/resources/Langues.java
@@ -0,0 +1,9 @@
+package fr.iut.montreuil.S4_R4_02_2023.num_50.joueur_sme.resources;
+
+public enum Langues {
+    FRANCAIS,
+    ENGLISH,
+    DEUTCH,
+    ESPANA,
+    ITALIA
+}
